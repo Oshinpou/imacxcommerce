dc8eaff800e79ff80e5a522948bd1e7c16624e1e
Oshinpou <oshinpounikar@gmail.com>
2025-09-25 15:54:43 +0530

    Create products-data.js

---
+++ products-data.js
@@ -0,0 +1,34 @@
+const allProducts = [
+  {
+    name: "Soleme Cheese",
+    price: 1,
+    category: "food",
+    subcategory: "breakfast",
+    image: "soleme-cheese.png",
+    description: "A soft breakfast cheese that melts in your mouth."
+  },
+  {
+    name: "Iconic Blue Chocolate",
+    price: 499,
+    category: "food",
+    subcategory: "chocolate",
+    image: "iconic-blue-chocolate.jpg",
+    description: "Rare artisan chocolate with rich blue cocoa notes."
+  },
+  {
+    name: "Grow Magic Jam",
+    price: 249,
+    category: "food",
+    subcategory: "breakfast",
+    image: "magic-jam.jpg",
+    description: "Floral jam spread that elevates morning mood."
+  },
+  {
+    name: "Need Crunchy Nuts",
+    price: 379,
+    category: "food",
+    subcategory: "snacks",
+    image: "crunchy-nuts.jpg",
+    description: "Nut blend with extra crunch for power snacks."
+  }
+];
